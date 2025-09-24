51c9e49f7cfe1d4de7a89de16a0b674f7a3c84da
Øystein Bringsli <oysteinmb@gmail.com>
2025-09-24 16:07:02 +0200

    Support for multiple devices

--- ControlCode/main.py
+++ ControlCode/main.py
@@ -15,15 +15,46 @@ response_label = tk.Label(root, text="ESP32 Response: ---")
 response_label.pack(pady=10)
 
 # Slider
-slider = tk.Scale(root, from_=-1000, to=1000, orient="horizontal", label="Target Position")
-slider.pack(pady=10)
+slider0 = tk.Scale(root, from_=0, to=5000, orient="horizontal", label="Target Position Wrist", length=800)
+slider0.pack(pady=10)
 
-def send_value(val):
+slider1 = tk.Scale(root, from_=-1000, to=1000, orient="horizontal", label="Target Position Shoulder", length=800)
+slider1.pack(pady=10)
+
+value0 = 0
+value1 = 0
+
+def send_value0(val):
+    global value0
+    value0 = float(val) * (-4.4)
     """Send slider value to ESP32"""
-    msg = f"{val}\n"   # match your ESP32 parser (comma-separated if more than one value)
+    msg = f"{value0},{value1}\n"   # match your ESP32 parser (comma-separated if more than one value)
     ser.write(msg.encode("utf-8"))
 
-slider.config(command=send_value)
+def send_value1(val):
+    global value1
+    value1 = float(val)
+    """Send slider value to ESP32"""
+    msg = f"{value0},{value1}\n"   # match your ESP32 parser (comma-separated if more than one value)
+    ser.write(msg.encode("utf-8"))
+
+slider0.config(command=send_value0)
+slider1.config(command=send_value1)
+
+def formatResponse(msg):
+    if ("," not in msg): return ""
+    v0, v1 = msg.split(",")
+    if (v0 == ""): return ""
+    if (v1 == ""): return ""
+    v0 = float(v0) / 4096.0 * 360.0 / (-4.4)
+    v1 = float(v1) / 4096.0 * 360.0
+
+    global value0, value1
+
+    d0 = v0 - value0
+    d1 = v1 - value1
+
+    return f"Shoulder: {v1:.2f} | Wrist: {v0:.2f}"
 
 def read_serial():
     """Background thread to read responses from ESP32"""
@@ -31,7 +62,7 @@ def read_serial():
         if ser.in_waiting:
             line = ser.readline().decode(errors="ignore").strip()
             if line:
-                response_label.config(text=f"ESP32 Response: {line}")
+                response_label.config(text=f"ESP32 Response: {formatResponse(line)}")
 
 # Run serial reader in a background thread
 thread = threading.Thread(target=read_serial, daemon=True)
