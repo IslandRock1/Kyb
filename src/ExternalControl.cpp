49b8f61bd8953476c64b358e2c007ce448eb7a57
Øystein Bringsli <oysteinmb@gmail.com>
2025-09-24 16:06:52 +0200

    Support for multiple devices

--- src/ExternalControl.cpp
+++ src/ExternalControl.cpp
@@ -6,28 +6,46 @@
 #include "SerialControl.hpp"
 #include "Config.hpp"
 
-DCMotor motor(MOTOR_PWM_PIN, MOTOR_IN1_PIN, MOTOR_IN2_PIN, PWM_CHANNEL);
-PID pid(2.5, 0.0, 0.0); // Kp 2.5
-SensorAS5600 sensor{I2C_SDA0_PIN, I2C_SCL0_PIN};
+DCMotor motor0{MOTOR0_PWM_PIN, MOTOR0_IN1_PIN, MOTOR0_IN2_PIN, PWM_CHANNEL0};
+DCMotor motor1{MOTOR1_PWM_PIN, MOTOR1_IN1_PIN, MOTOR1_IN2_PIN, PWM_CHANNEL1};
+PID pid0(2.5, 0.0, 0.0); // Kp 2.5
+PID pid1(2.5, 0.0, 0.0);
+SensorAS5600 sensor0{I2C_SDA0_PIN, I2C_SCL0_PIN, 0};
+SensorAS5600 sensor1{I2C_SDA1_PIN, I2C_SCL1_PIN, 1};
 SerialControl serial_control;
 
 unsigned long startup_time;
 void setup() {
     startup_time = millis();
 
-    sensor.begin();
-    motor.begin();
+    sensor0.begin();
+    sensor1.begin();
+
+    sensor0.resetCumulativePosition();
+    sensor1.resetCumulativePosition();
+
+    motor0.begin();
+    motor1.begin();
 }
 
 void loop() {
     serial_control.update();
-    pid.setTarget(serial_control.inputData.targetPosition0);
-    int32_t currentAngle_steps = sensor.getCumulativePosition();
-    serial_control.outputData.currentPosition0 = currentAngle_steps;
-    double currentAngle = static_cast<double>(currentAngle_steps) * SensorAS5600::RAW_TO_DEG;
+    pid0.setTarget(serial_control.inputData.targetPosition0);
+    pid1.setTarget(serial_control.inputData.targetPosition1);
 
-    double motorPower = pid.calculate(currentAngle);
-    motor.move(static_cast<int>(motorPower));
+    int32_t currentAngle_steps0 = sensor0.getCumulativePosition();
+    int32_t currentAngle_steps1 = sensor1.getCumulativePosition();
+
+    serial_control.outputData.currentPosition0 = currentAngle_steps0;
+    serial_control.outputData.currentPosition1 = currentAngle_steps1;
+
+    double currentAngle0 = static_cast<double>(currentAngle_steps0) * SensorAS5600::RAW_TO_DEG;
+    double currentAngle1 = static_cast<double>(currentAngle_steps1) * SensorAS5600::RAW_TO_DEG;
+
+    double motorPower0 = pid0.calculate(currentAngle0);
+    double motorPower1 = pid1.calculate(currentAngle1);
+    motor0.move(static_cast<int>(motorPower0));
+    motor1.move(static_cast<int>(motorPower1));
 
     /*Serial.print("Target: ");
     Serial.print(pid.getTarget());
