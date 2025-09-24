08dfe505d9e34954ab4d72860bc530557a061bb6
Øystein Bringsli <oysteinmb@gmail.com>
2025-09-24 16:06:02 +0200

    Fixed usage of multiple motors

--- lib/Config/Config.hpp
+++ lib/Config/Config.hpp
@@ -2,13 +2,17 @@
 #ifndef TESTRIG_CONFIG_HPP
 #define TESTRIG_CONFIG_HPP
 
-constexpr int MOTOR_PWM_PIN = 25;
-constexpr int MOTOR_IN1_PIN = 26;
-constexpr int MOTOR_IN2_PIN = 14;
+constexpr int MOTOR0_PWM_PIN = 25;
+constexpr int MOTOR0_IN1_PIN = 26;
+constexpr int MOTOR0_IN2_PIN = 14;
+constexpr int MOTOR1_PWM_PIN = 33;
+constexpr int MOTOR1_IN1_PIN = 32;
+constexpr int MOTOR1_IN2_PIN = 13;
 constexpr  int I2C_SDA0_PIN = 5;
 constexpr  int I2C_SCL0_PIN = 17;
-constexpr  int I2C_SDA1_PIN = 21;
-constexpr  int I2C_SCL1_PIN = 17;
-constexpr  int PWM_CHANNEL = 0;
+constexpr  int I2C_SDA1_PIN = 19;
+constexpr  int I2C_SCL1_PIN = 18;
+constexpr  int PWM_CHANNEL0 = 0;
+constexpr  int PWM_CHANNEL1 = 1;
 
 #endif //TESTRIG_CONFIG_HPP
